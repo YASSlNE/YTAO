588bf35dd3c8f02eb11195619d2b003ce470faa4
YASSlNE <mdyassinechiboub@gmail.com>
2022-08-06 23:47:46 +0100

    react part is done, not pretty

--- ytao/settings.py
+++ ytao/settings.py
@@ -40,17 +40,22 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'rest_framework',
     'ytao_api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True   
+
 
 ROOT_URLCONF = 'ytao.urls'
 
