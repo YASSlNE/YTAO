fe3d1033471a18e6289c677f7bb6dcca8213ec08
YASSlNE <mdyassinechiboub@gmail.com>
2022-08-07 17:44:48 +0100

    deploying to heroku

--- ytao/settings.py
+++ ytao/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w-t$osjuswca$it38+u*f17fc^dojbx&te$%e_tvx5!1*@6rve
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
